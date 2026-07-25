00ff88e5233f8c9d4e4bb4426ad809dcd206bd85
juliusneilj <juliusneiljumawan@gmail.com>
2026-07-25 23:22:59 +1000

    feat: update BasePage constructor to include baseUrl parameter

--- src/core/base-page.ts
+++ src/core/base-page.ts
@@ -2,9 +2,11 @@ import { Page } from "playwright/test";
 
 export default abstract class BasePage {
    protected page: Page;
+   protected baseUrl: string;
    
-   constructor(page: Page) {
+   constructor(page: Page, baseUrl: string, timeout: number = 10000) {
       this.page = page;
+      this.baseUrl = baseUrl;
    }
 
    abstract init() : Promise<this>;
