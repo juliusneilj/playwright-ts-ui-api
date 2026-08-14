a4071b1d594ab2c78dad44cd646cebfd604db386
juliusneilj <juliusneiljumawan@gmail.com>
2026-08-14 17:22:37 +1000

    feat: add API request types and retail website types for improved type safety

---
+++ src/types/retail-website.types.ts
@@ -0,0 +1,2 @@
+export type ProductCategory = 'gifts' | 'kids' | "men" | "women"; // Add more categories as needed
+export type ProductSize = 'XS' | 'S' | 'M' | 'L' | 'XL' | 'XXL'; // Add more sizes as needed
