60c616d781fd2ee5ecd0b3c5475f5e13067da649
juliusneilj <juliusneiljumawan@gmail.com>
2026-08-14 17:21:53 +1000

    feat: add AutomationExerciseAPI and PokemonAPI classes for API interactions

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,2 +1,2 @@
-export { default as PokemonAPI } from './pokemon-api';
-export { default as AutomationExerciseAPI } from './automation-exercise-api';
+export { default as PokemonAPI } from './pokemon.api';
+export { default as AutomationExerciseAPI } from './automation-exercise.api';
